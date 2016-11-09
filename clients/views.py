bf37117ef2c4a2955e969f7c4134e5d1683a109e
Michael Cranston <cranston.michaelrichard@gmail.com>
2016-11-09 15:58:21 -0500

    #8 only return projects that belong to the user

--- clients/views.py
+++ clients/views.py
@@ -1,15 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
 
 from clients.models import Client
-from clients.permissions import ClientPermission
 from clients.serializers import ClientSerializer
 
 
 class ClientViewSet(ModelViewSet):
     serializer_class = ClientSerializer
-    permission_classes = [ClientPermission]
 
     def get_queryset(self):
-        return Client.objects.filter(
-            user__id = self.request.user.id
+        queryset = Client.objects.filter(
+            user__id=self.request.user.id
         )
+
+        return queryset
