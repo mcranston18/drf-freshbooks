33469e63624ad874dd6d838d44f3caaded631f9f
Michael Cranston <cranston.michaelrichard@gmail.com>
2016-11-10 17:04:22 -0500

    #16 update api urls

--- freshclone/urls.py
+++ freshclone/urls.py
@@ -9,9 +9,15 @@ from freshclone.routers import router
 
 BASE_API_URL = 'api/v1'
 
+api_urls = [
+    url(
+        r'^o/',
+        include('oauth2_provider.urls', namespace='oauth2_provider')),
+    url('', include(router.urls)),
+    url(r'^docs/', schema_view)
+]
+
 urlpatterns = [
-    url(r'^api/v1/', include(router.urls)),
-    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    url(r'^api/v1/', include(api_urls)),
     url(r'^admin/', admin.site.urls),
-    url(r'^api/v1/docs/', schema_view)
 ]
