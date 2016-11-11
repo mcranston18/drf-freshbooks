649873d7f6f1eb9be2771b7253097a9182c02e65
Michael Cranston <cranston.michaelrichard@gmail.com>
2016-11-11 10:41:26 -0500

    #16 use different serializer for list vs detail view

--- users/serializers.py
+++ users/serializers.py
@@ -2,11 +2,22 @@ from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserListSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = [
+            'username',
+            'id',
+        ]
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
+        fields = '__all__'
--- users/views.py
+++ users/views.py
@@ -1,20 +1,25 @@
 from django.contrib.auth.models import User, Group
 
 from rest_framework import permissions, viewsets
+from rest_framework.response import Response
 
 from oauth2_provider.ext.rest_framework import (
     TokenHasReadWriteScope,
     TokenHasScope,
 )
 
-from .serializers import UserSerializer, GroupSerializer
+from .serializers import UserListSerializer, UserDetailSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    permission_classes = [permissions.IsAuthenticated]
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = UserDetailSerializer
 
+    def list(self):
+        queryset = self.get_queryset()
+        serializer = UserListSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 class GroupViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, TokenHasScope]
