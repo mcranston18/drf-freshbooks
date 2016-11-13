6d3262e29b3db20e4a54167b2c5a842573b5d0c7
Michael Cranston <cranston.michaelrichard@gmail.com>
2016-11-13 16:28:46 -0500

    #18 adds custom filter set to projects

--- projects/views.py
+++ projects/views.py
@@ -1,14 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 
+from projects.filters import ProjectFilter
 from projects.models import Project
 from projects.serializers import ProjectSerializer
 
 
 class ProjectViewSet(ModelViewSet):
     serializer_class = ProjectSerializer
-
+    # filter_fields = ('status', 'budget')
+    filter_class = ProjectFilter
     def get_queryset(self):
         queryset = Project.objects.filter(
             client__user__id=self.request.user.id
