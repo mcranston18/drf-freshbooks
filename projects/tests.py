c61c76ed9b8438e5175ad1d660630c8188fe1a30
Michael Cranston <cranston.michaelrichard@gmail.com>
2016-11-13 16:59:52 -0500

    #18 adds tests to projects

--- projects/tests.py
+++ projects/tests.py
@@ -16,9 +16,23 @@ class APITests(APITestCase):
             user=self.user
         )
 
+        self.other_sample_client_object = mommy.make(
+            'Client',
+            user=self.user
+        )
+
         self.project = mommy.make(
             'Project',
             client=sample_client_object,
+            budget=1000,
+            status='active'
+        )
+
+        self.other_project = mommy.make(
+            'Project',
+            client=self.other_sample_client_object,
+            budget=100000,
+            status='inactive',
         )
 
         self.project_data = {
@@ -46,6 +60,50 @@ class APITests(APITestCase):
             )
         )
         self.assertEqual(response.status_code, 200)
+
+    def test_get_products_filter_by_client(self):
+        client_id = self.other_sample_client_object.id
+        response = self.authenticated_client.get(
+            reverse('project-list') + '?client=%s' % client_id
+        )
+        projects = response.json()
+
+
+        for project in projects:
+            self.assertEqual(project['client'], client_id)
+
+    def test_get_products_filter_by_status(self):
+        status = 'active'
+        response = self.authenticated_client.get(
+            reverse('project-list') + '?status=%s' % status
+        )
+        projects = response.json()
+
+
+        for project in projects:
+            self.assertEqual(project['status'], status)
+
+    def test_get_products_filter_by_min_budget(self):
+        min_budget = 10000
+        response = self.authenticated_client.get(
+            reverse('project-list') + '?min_budget=%s' % min_budget
+        )
+        projects = response.json()
+
+
+        for project in projects:
+            self.assertTrue(project['budget'] > min_budget)
+
+    def test_get_products_filter_by_max_budget(self):
+        max_budget = 10000
+        response = self.authenticated_client.get(
+            reverse('project-list') + '?max_budget=%s' % max_budget
+        )
+        projects = response.json()
+
+
+        for project in projects:
+            self.assertTrue(project['budget'] < max_budget)
 
     def test_post_projects_returns_201(self):
         response = self.authenticated_client.post(
