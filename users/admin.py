dd0dda27f5d82d3ebe08a1b7220f88175ba3011c
Michael Cranston <cranston.michaelrichard@gmail.com>
2016-11-17 08:10:08 -0500

    #20 adds favorite colour to user model

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
-from .models import User
+User = get_user_model()
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    def __init__(self, *args, **kwargs):
+        super(CustomUserAdmin, self).__init__(*args, **kwargs)
+        self.list_display = list(UserAdmin.list_display) + [
+            'favourite_colour'
+        ]
+
+
+admin.site.register(User, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,5 +1,9 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    favourite_colour = models.CharField(
+        max_length=30,
+        blank=True,
+    )
