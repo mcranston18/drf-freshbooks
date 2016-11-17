adca6116898545d826f86d1e75926efb0cc15637
Michael Cranston <cranston.michaelrichard@gmail.com>
2016-11-17 11:28:40 -0500

    23 add client contact model

--- clients/models.py
+++ clients/models.py
@@ -2,19 +2,6 @@ from django.db import models
 from django.conf import settings
 
 
-class ClientContact(models.Model):
-    name = models.CharField(
-        max_length=50,
-        blank=True,
-        help_text='The name of the contact associated with the client'
-    )
-    email = models.EmailField(
-        unique=True,
-        blank=True,
-        help_text='The e-mail of the contact associated with the client'
-    )
-
-
 class Client(models.Model):
     CLIENT_TYPE_CHOICES = [
         ('startup', 'Startup'),
@@ -24,11 +11,17 @@ class Client(models.Model):
     name = models.CharField(max_length=30)
     client_type = models.CharField(max_length=10, choices=CLIENT_TYPE_CHOICES)
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    contacts = models.ForeignKey(
-        ClientContact,
-        blank=True,
-        null=True,
-    )
 
     def __str__(self):
         return self.name
+
+
+class ClientContact(models.Model):
+    name = models.CharField(
+        max_length=50,
+        help_text='The name of the contact associated with the client',
+    )
+    email = models.EmailField(
+        help_text='The e-mail of the contact associated with the client',
+    )
+    client = models.ForeignKey(Client)
