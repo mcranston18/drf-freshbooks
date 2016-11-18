0a8f6a2870f5dc32f36923b6ddcd3729183c4bc5
Michael Cranston <cranston.michaelrichard@gmail.com>
2016-11-18 11:28:33 -0500

    22 adds invoice serializer and views

--- invoices/views.py
+++ invoices/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework_extensions.mixins import NestedViewSetMixin
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Invoice
+from .serializers import InvoiceSerializer
+
+
+class InvoiceViewSet(ModelViewSet):
+    serializer_class = InvoiceSerializer
+    # queryset = Invoice.objects.all()
+
+    def get_queryset(self):
+        # user_id = invoice.clients.user
+        queryset = Invoice.objects.filter(
+            client__user__id=self.request.user.id
+        )
+
+        return queryset
