1867c0ab4e5b74f7033537e6b17710107c8fb05f
Michael Cranston <cranston.michaelrichard@gmail.com>
2016-11-18 11:01:42 -0500

    #22 adds invoice model

--- freshclone/settings.py
+++ freshclone/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'users',
     'clients',
+    'invoices',
     'projects',
 ]
 
---
+++ invoices/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+from clients.models import Client
+from projects.models import Project
+
+
+class Invoice(models.Model):
+    client = models.ForeignKey(Client)
+    project = models.ManyToManyField(
+        Project,
+        related_name='invoices'
+    )
+    date_of_issue = models.DateField()
+    amount = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+    )
+    tax_rate = models.DecimalField(
+        max_digits=4,
+        decimal_places=2,
+    )
+    attachments = models.FileField(
+        upload_to='uploads/',
+    )
+    name = models.CharField(max_length=500)
+    comment = models.CharField(max_length=500)
+    terms = models.CharField(max_length=5000)
+
+    @property
+    def total_amount(self):
+        tax_amount = (self.tax_rate / self.amount) * 100
+        return self.amount + tax_amount
+
+    def __str__(self):
+        return self.name
